219a494b63c2fff48e63ad97b47d28e6b3a10e5d
sergiogval <sergioval@gmail.com>
2022-01-05 02:32:27 -0600

    Add style to both screen sizes

--- js/speakers.js
+++ js/speakers.js
@@ -1,27 +1,60 @@
 const arr = [
   {
-    id: 0, image: 'images/speaker1.png', name: 'Usaian Bolt', title: 'Holder of various running world records',
+    id: 0,
+    image: 'images/speaker1.png',
+    name: 'Usaian Bolt',
+    title: 'Holder of various running world records',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
   {
-    id: 1, image: 'images/speaker2.png', name: 'Elon Musk', title: 'excentric multimillonaire',
+    id: 1,
+    image: 'images/speaker2.png',
+    name: 'Elon Musk',
+    title: 'excentric multimillonaire',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
   {
-    id: 2, image: 'images/speaker3.png', name: 'Ian Thorpe', title: 'The olimpyc swwimer with more Gold Medals in the history',
+    id: 2,
+    image: 'images/speaker3.png',
+    name: 'Ian Thorpe',
+    title: 'The Olympic swimmer with more Gold Medals in the history',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
   {
-    id: 3, image: 'images/speaker4.png', name: 'James Franco', title: 'Actor and Hemp activist',
+    id: 3,
+    image: 'images/speaker4.png',
+    name: 'James Franco',
+    title: 'Actor and Hemp activist',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
   {
-    id: 4, image: 'images/speaker6.png', name: 'Bob Marley', title: 'Reggue singer and Hemp activist',
+    id: 4,
+    image: 'images/speaker6.png',
+    name: 'Bob Marley',
+    title: 'Reggae singer and Hemp activist',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
   {
-    id: 5, image: 'images/speaker5.png', name: 'Maria Sabina', title: 'Mexican ancent culture sacerdotice',
+    id: 5,
+    image: 'images/speaker5.png',
+    name: 'Maria Sabina',
+    title: 'Mexican ancien culture sacerdotice',
+    description: 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut',
   },
 ];
 
 for (let i = 0; i <= arr.length; i += 1) {
   const card = document.createElement('div');
   card.className = 'card';
-  card.innerHTML = `<div id="speakers"><div class="card-content"><img src="${arr[i].image}"><h3>${arr[i].name}</h3><p>${arr[i].title}</p></div></div>`;
+  card.innerHTML = `
+  <div class="cardContent">
+  <img class="speakerImg" src="${arr[i].image}" alt="${arr[i].name}"  style="background-image: url('../images/chess.png');">  
+  <div class="cardBody">
+  <h4 class="speakerName">${arr[i].name}</h3>
+  <p class="speakerBio">${arr[i].title}</p>
+  <p class="speakerDescription">${arr[i].description}</p>
+  </div>
+  </div>
+  </div>`;
   document.getElementById('speakers').appendChild(card);
 }
